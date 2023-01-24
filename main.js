316c6a5f6c7a1cba537b5c20752fc81860d97f82
Aram Ezalddin <etec02m210260@epu.edu.iq>
2023-01-24 21:31:09 +0300

    last update

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ window.addEventListener('load' , () => {
 const form = document.querySelector("#new-task-form");
 const input = document.querySelector("#new-task-input");
 const list_el = document.querySelector("#tasks");
+const button = document.querySelector(".btn");
+button.addEventListener('click' , (e) =>  {
+    window.alert("hello");
+});
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
